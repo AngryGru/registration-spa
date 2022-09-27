f783d99ab8139db2e49a8c03352c400983ecc528
Anna Gruzd <ann.gruzd97.ag@gmail.com>
2022-09-27 10:24:20 +0300

    add registration-page component

---
+++ src/pages/Registration/Registration.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Registration.css";
+
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Registration() {
+  return (
+    <div className="RegistrationContainer">
+      <p>HELLO</p>
+      <Button variant="primary">Click</Button>
+    </div>
+  );
+}
+
+export default Registration;
