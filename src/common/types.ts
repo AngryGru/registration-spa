30b34f770692592ac6729e2f24618e576768db57
Anna Gruzd <ann.gruzd97.ag@gmail.com>
2022-09-29 16:33:54 +0300

    remove types file

--- src/common/types.ts
+++
@@ -1,9 +0,0 @@
-export type InputProps = {
-  type: string;
-  name?: string;
-  className?: string;
-  placeholder?: string;
-  value?: any;
-  onBlur?: (event: any) => void;
-  onChange?: (event: any) => void;
-};
